40bc594baa844c3e2807fe237cdf4419a5430d6f
Adam Neal <aneal@gravitatesolutions.com>
2017-11-01 15:36:56 -0500

    Improve file handling code.

--- import_fortune_file.py
+++ import_fortune_file.py
@@ -1,21 +1,19 @@
 import boto3
 
 def import_file():
-    f = open("fortunes.txt", "r")
-    i = 0
-    author = quote = ""
+    with open("fortunes.txt", "rt") as f:
+        author = quote = ""
 
-    for line in f:
-        line = line.strip()
+        for i, line in enumerate(f):
+            line = line.strip()
 
-        if line.startswith("~") or line.startswith("-"):
-            author = line.strip("~- ")
-        elif line == "%":
-            i += 1
-            insert_record(i, quote, author)
-            author = quote = ""
-        else:
-            quote += " " + line.strip('"')
+            if line.startswith("~") or line.startswith("-"):
+                author = line.strip("~- ")
+            elif line == "%":
+                insert_record(i, quote, author)
+                author = quote = ""
+            else:
+                quote += " " + line.strip('"')
 
 
 def insert_record(fortune_id, quote, author):
